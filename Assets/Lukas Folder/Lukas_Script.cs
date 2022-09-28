a614605e22e059e71ec244c1d6c4656ec8d50d85
l2xu <weihrauch.business@gmail.com>
2022-09-28 20:10:41 +0200

    added random stuff

--- Assets/Lukas Folder/Lukas_Script.cs
+++ Assets/Lukas Folder/Lukas_Script.cs
@@ -7,7 +7,8 @@ public class Lukas_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Lukas hat vielleicht CORONA, YIHAAAA")
+        Debug.Log("Ich bin ein Auto, katchau!");
+        Debug.Log("Lukas hat vielleicht CORONA, YIHAAAA");
     }
 
     // Update is called once per frame
