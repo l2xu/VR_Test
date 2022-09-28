fad7b3643940eb2713db1a7c9286dab965b5451c
l2xu <weihrauch.business@gmail.com>
2022-09-28 20:06:11 +0200

    added folder and script

---
+++ Assets/Lukas Folder/Lukas_Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lukas_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Lukas hat vielleicht CORONA, YIHAAAA")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
