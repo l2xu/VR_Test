9aaad268ba232de60ce669b61064b8b473fae0f1
Windixi <u36515@hs-harz.de>
2022-09-28 20:11:22 +0200

    test_hanne
    test_hanne

--- Assets/Lukas Folder/Lukas_Script.cs
+++ Assets/Lukas Folder/Lukas_Script.cs
@@ -7,7 +7,8 @@ public class Lukas_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Lukas hat vielleicht CORONA, YIHAAAA")
+        Debug.Log("Lukas hat vielleicht CORONA, YIHAAAA");
+        Debug.Log("Gute Besserung");
     }
 
     // Update is called once per frame
